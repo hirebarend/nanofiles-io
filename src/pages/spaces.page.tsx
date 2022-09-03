a1e020133eb8d847d21c7d4d980552cea9909f92
Barend Erasmus <hirebarend@gmail.com>
2022-09-03 21:26:33 +0200

    update

--- src/pages/spaces.page.tsx
+++ src/pages/spaces.page.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faDownload, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export function SpacesPage() {
   return (
@@ -10,7 +10,7 @@ export function SpacesPage() {
       <div className="tw-px-6 tw-py-36">
         <div className="tw-max-w-xl tw-mx-auto">
           <div className="tw-mb-3 tw-text-center">Your space ID is</div>
-          <div className="tw-mb-1 tw-text-center">
+          <div className="tw-mb-6 tw-text-center">
             <span className="tw-bg-black tw-px-2.5 tw-py-2.5 tw-rounded-full tw-text-white">
               1234321&nbsp;&nbsp;
               <button>
@@ -18,6 +18,23 @@ export function SpacesPage() {
               </button>
             </span>
           </div>
+
+          {[0, 0, 0].map((x, index) => (
+            <div
+              className="tw-bg-gray-50 tw-flex tw-gap-4 tw-justify-between tw-my-3 tw-px-2.5 tw-py-2.5 tw-rounded-lg"
+              key={index}
+            >
+              <div>IMG_0339.jpeg</div>
+              <div>
+                <button className="tw-mr-1 tw-text-red-500">
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+                <button className="tw-ml-1 tw-text-primary">
+                  <FontAwesomeIcon icon={faDownload} />
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
