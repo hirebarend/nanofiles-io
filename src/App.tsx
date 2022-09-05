1dc8273bf8b4aef717a54e44b67152e19c234d6c
Barend Erasmus <hirebarend@gmail.com>
2022-09-05 09:28:08 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import mixpanel from "mixpanel-browser";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { HomePage, SpacesPage } from "./pages";
 
 function App() {
+  const location = useLocation();
+
   useEffect(() => {
     mixpanel.init("6570fb6b55412e8145762b070dd25c3b");
 
     mixpanel.track("Page View");
-  }, []);
+  }, [location]);
 
   return (
     <Routes>
